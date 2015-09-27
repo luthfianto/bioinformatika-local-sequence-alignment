d83e708612f1e43478615993cbf95f63fa85c7b0
Rizky Luthfianto <mrluthfianto@gmail.com>
2015-09-27 10:34:47 +0700

    feat: nambah untuk multisequence. mungkin dah selesai

--- blast.py
+++ blast.py
@@ -1,8 +1,10 @@
 from operator import itemgetter
-from Bio.SubsMat.MatrixInfo import blosum62
+from Bio.SubsMat.MatrixInfo import blosum62 #import tabel blosum62
 
-A='MMIIPKLM'
+A='MMIIPKLQ'
 B='AAPKLMMQ'
+D='AAKKLMMQ'
+C=[B,D]
 
 #fungsi untuk mendapatkan k-mers dari sequence
 def kmers(string, k):
@@ -11,70 +13,87 @@ def kmers(string, k):
 
 #fungsi untuk mendapatkan skor 3-mers dengan matriks blosum62 dari kedua sequence
 def score(A, B):
-    scores = [ blosum62.get((A[i], B[i]), blosum62.get((B[i], A[i]))) for i in range(len(A)) ]
+    scores = [
+        #get(A,B) dan juga get(B,A), karena berbentuk tabel blosum hanya berbentuk matriks segitiga
+        blosum62.get((A[i], B[i]), blosum62.get((B[i], A[i])))
+        for i in range(len(A))
+    ]
+    
     return sum(scores)
+
+#algoritma inti dari program blast ini    
+def blastpair(A,B):
     
-#Bentuk 3-mers dari tiap sequence
-a=kmers(A,3)
-b=kmers(B,3)
-
-#Tentukan titik 3-mer tengah terbaik berdasar skor
-centerscorelist=[]
-for i_key,i in a:
-    for j_key,j in b:
-        centerscorelist.append((i_key,j_key,i,j,score(i,j)))
-
-#pilih 3-mers dengan skor tertinggi
-center=max(centerscorelist,key=itemgetter(4))
-print(center)
-centerscore=center[4]
-a_index, b_index=center[0], center[1]
-
-#iterasi dari tengah ke kiri dan ke kanan
-
-#kiri
-left=min(len( A[0:a_index+3]), len(B[0:b_index+3])); left
-
-a_left=A[a_index-(left-3):a_index]
-b_left=B[b_index-(left-3):b_index]
-
-leftscorelist=[]
-for i in range(len(a_left)):
-    temp=(a_left[i], b_left[i],score(a_left[i], b_left[i]))
-    if temp[2]>=0:
-        leftscorelist.append(temp[2])
-        left_index=i
-    else:
-        break
+    #Bentuk 3-mers dari tiap sequence
+    a=kmers(A,3)
+    b=kmers(B,3)
+    #Tentukan titik 3-mer tengah terbaik berdasar skor
+    centerscorelist=[]
+    for i_key,i in a:
+        for j_key,j in b:
+            centerscorelist.append((i_key,j_key,i,j,score(i,j)))
     
-leftscoresum=sum(leftscorelist)
-print(leftscoresum)
-
-#kanan
-right=min(len( A[a_index:]), len(B[b_index:])); right
-a_right=A[a_index+3:a_index+right]
-b_right=B[b_index+3:b_index+right]
-
-rightscorelist=[]
-
-for i in range(len(a_right)):
-    temp=(a_right[i], b_right[i],score(a_right[i], b_right[i]))
-    if temp[2]>=0:
-        rightscorelist.append(temp[2])
-        right_index=i
-    else:
-        break
+    #pilih 3-mers dengan skor tertinggi
+    center=max(centerscorelist,key=itemgetter(4))
+    #print(center)
+    centerscore=center[4]
+    a_index, b_index=center[0], center[1]
     
-sum(rightscorelist)
-rightscoresum=sum(rightscorelist)
-print(rightscoresum)
-total_score = leftscoresum + centerscore + rightscoresum
-#print(total_score)
+    ## iterasi dari tengah ke kiri dan ke kanan
+    left_index = 0
+    right_index= 0
+    
+    # iterasi kiri
+    left=min(len( A[0:a_index+3]), len(B[0:b_index+3])); left
+    
+    a_left=A[a_index-(left-3):a_index]
+    b_left=B[b_index-(left-3):b_index]
+    #print(b_index)
+    
+    leftscorelist=[]
+    for i in range(len(a_left)):
+        temp=(a_left[i], b_left[i],score(a_left[i], b_left[i]))
+        if temp[2]>=0:
+            leftscorelist.append(temp[2])                        
+        else:
+            break
+        
+    leftscoresum=sum(leftscorelist)
+    #print(leftscoresum)
+    
+    # iterasi kanan
+    right=min(len( A[a_index:]), len(B[b_index:])); right
+    a_right=A[a_index+3:a_index+right]
+    b_right=B[b_index+3:b_index+right]
+    
+    rightscorelist=[]
+    
+    for i in range(len(a_right)):
+        temp=(a_right[i], b_right[i],score(a_right[i], b_right[i]))
+        if temp[2]>=0:
+            rightscorelist.append(temp[2])
+        else:
+            break
+        
+    rightscoresum=sum(rightscorelist)
+    #print(rightscoresum)
+    total_score = leftscoresum + centerscore + rightscoresum
+    #print(total_score)
+    
+    a_kiri=a_index-len(leftscorelist)
+    a_kanan=a_index+3+len(rightscorelist)
+    b_kiri=b_index-len(leftscorelist)
+    b_kanan=b_index+3+len(rightscorelist)
+    
+    [print('.', end="") for i in range(b_index+2)]
+    print(B[b_kiri:b_kanan], total_score, " <-", B)
+    
+#blastpair(A,B)
 
-a_kiri=a_index-len(leftscorelist)
-a_kanan=a_index+3+len(rightscorelist)
-b_kiri=b_index-len(leftscorelist)
-b_kanan=b_index+3+len(rightscorelist)
-print(A[a_kiri:a_kanan])
-print(B[b_kiri:b_kanan], total_score)
-#
+def blastdb(query, db):
+    #jalankan blastpair pada tiap sequence dalam array/db
+    print(query, '<- query')
+    for dbsequence in db:        
+        blastpair(query, dbsequence)
+        
+blastdb(A,C)
